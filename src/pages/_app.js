05db8d274d9ae00922cfc41cb8352fe3de5d2afe
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-16 03:36:28 +0300

    add meta data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
 import '@/styles/globals.css';
 import { Poppins } from 'next/font/google';
+import Head from 'next/head';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400'] });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={poppins.className}>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <link rel='shortcut icon' type='image/png' href='/img/icon.png' />
+
+        <title>Bankist | When Banking meets Minimalist</title>
+      </Head>
       <Component {...pageProps} />
     </main>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,10 @@ import Header from '@/components/header';
 import OperationSection from '@/components/operation/OperationSection';
 import Signup from '@/components/signup';
 import Testimonial from '@/components/testimonial';
-import Head from 'next/head';
-import React from 'react';
 
 function index() {
   return (
     <>
-      <Head>
-        <title>Bankist</title>
-      </Head>
       <Header />
       <Feature />
       <OperationSection />
